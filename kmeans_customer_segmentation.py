2f0adfc323b6573c67a09a1c3fbee4d285c7d4f8
Sahil Goyal <itzsahilg1@gmail.com>
2026-06-10 18:21:25 +0530

    Implement KMeans customer segmentation analysis

---
+++ kmeans_customer_segmentation.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+
+# Load dataset
+dataset = pd.read_csv('Mall_Customers.csv')
+
+# Select features (Annual Income and Spending Score)
+X = dataset.iloc[:, [3,4]].values
+
+# Finding optimal clusters using Elbow Method
+wcss = []
+
+for i in range(1,11):
+    kmeans = KMeans(n_clusters=i, init='k-means++', random_state=42)
+    kmeans.fit(X)
+    wcss.append(kmeans.inertia_)
+
+# Plot elbow graph
+plt.plot(range(1,11), wcss)
+plt.title('Elbow Method')
+plt.xlabel('Number of clusters')
+plt.ylabel('WCSS')
+plt.show()
+
+# Apply KMeans
+kmeans = KMeans(n_clusters=5, init='k-means++', random_state=42)
+y_kmeans = kmeans.fit_predict(X)
+
+# Plot clusters
+plt.scatter(X[y_kmeans == 0,0], X[y_kmeans == 0,1], s=100, label='Cluster 1')
+plt.scatter(X[y_kmeans == 1,0], X[y_kmeans == 1,1], s=100, label='Cluster 2')
+plt.scatter(X[y_kmeans == 2,0], X[y_kmeans == 2,1], s=100, label='Cluster 3')
+plt.scatter(X[y_kmeans == 3,0], X[y_kmeans == 3,1], s=100, label='Cluster 4')
+plt.scatter(X[y_kmeans == 4,0], X[y_kmeans == 4,1], s=100, label='Cluster 5')
+
+plt.scatter(kmeans.cluster_centers_[:,0], kmeans.cluster_centers_[:,1],
+            s=300, c='yellow', label='Centroids')
+
+plt.title('Customer Segmentation')
+plt.xlabel('Annual Income')
+plt.ylabel('Spending Score')
+plt.legend()
+plt.show()
